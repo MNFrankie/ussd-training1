349664fe8282404c128c058a5e15f7796af25f5f
MNFrankie <francismuriithi15@gmail.com>
2015-08-24 18:19:08 +0300

    added files

--- examples/mshwari.php
+++ examples/mshwari.php
@@ -1,11 +1,14 @@
-
 <?php
 //text bt
+
 //text is blank = Level 0
 //second text is not empty but does have * = level 1
 //third text has one star = level 2
+
 $text= $_REQUEST['text'];
+
 $result =  getLevel($text);
+
 $level = $result['level'];
 $message = $result['latest_message'];
 switch (strtolower($level)) {
@@ -22,19 +25,28 @@ switch (strtolower($level)) {
      $response = getHomeMenu();
     break;
 }
+
+
+
 sendOutput($response,1);
 exit;
+
 $exploded_text = explode('*',$text);
+
 print_r($exploded_text);
 exit;
+
+
 //1*1*3*5
 $input = getInput();
+
 if ( $input['text'] == "" ) {
      // This is the first request. Note how we start the response with CON
      $response  = "1. Loan".PHP_EOL;
      $response  .= "2. M-Shwari Balance";
 	 sendOutput($response,1);
 }else{
+
   switch (strtolower($input['text'])) {
       case 1:
       $response  = "1. Request Loan".PHP_EOL;
@@ -50,11 +62,13 @@ if ( $input['text'] == "" ) {
   }
   sendOutput($response,1);
 }
+
 function getLevelOneMenu($text){
+
   switch (strtolower($text)) {
       case 1:
       $response  = "1. Request Loan".PHP_EOL;
-      $response  .= "1. Pay Loan";
+      $response  .= "2. Pay Loan";
           break;
       case 2:
        $response = "Your balance is Ksh. 235. Thanks for using MShwari";
@@ -65,12 +79,15 @@ function getLevelOneMenu($text){
         break;
   }
   return $response;
+
 }
 function getHomeMenu(){
   $response  = "1. Loan".PHP_EOL;
   $response  .= "2. M-Shwari Balance";
+
   return $response;
 }
+
 //verify if the id belongs to one of the staff members
 function getInput(){
 $input = array();
@@ -78,7 +95,9 @@ $input['sessionId']   = $_REQUEST["sessionId"];
 $input['serviceCode'] = $_REQUEST["serviceCode"];
 $input['phoneNumber'] = $_REQUEST["phoneNumber"];
 $input['text']        = $_REQUEST["text"];
+
 return $input;
+
 }
 function getLevel($text){
   if($text == ""){
@@ -87,11 +106,13 @@ function getLevel($text){
     $exploded_text = explode('*',$text);
     $response['level'] = count($exploded_text);
     $response['latest_message'] = end($exploded_text);
+
   }
   return $response;
 }
 function sendOutput($message,$type=2){
 	//Type 1 is a continuation, type 2 output is an end
+
 	if($type==1){
 		echo "CON ".$message;
 	}elseif($type==2){
@@ -101,4 +122,5 @@ function sendOutput($message,$type=2){
 	}
 	exit;
 }
+
 ?>
